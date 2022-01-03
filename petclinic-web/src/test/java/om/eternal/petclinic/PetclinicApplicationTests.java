9346708f0c45d83821d2532ed9e1c017f31eb28b
Abhishek-Maurya <abhishek.maurya@coreviewsystems.com>
2022-01-03 17:42:06 +0530

    adding multimodule structure. Closes #1

---
+++ petclinic-web/src/test/java/om/eternal/petclinic/PetclinicApplicationTests.java
@@ -0,0 +1,16 @@
+//package om.eternal.petclinic;
+//
+//
+//import org.junit.jupiter.api.Test;
+//import org.junit.Ignore;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class PetclinicApplicationTests {
+//
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
