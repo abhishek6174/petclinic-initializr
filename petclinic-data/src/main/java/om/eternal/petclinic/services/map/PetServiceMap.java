c17f9987ef2353aadfeb91ac4f0fac37586c80e7
Abhishek-Maurya <abhishek.maurya@coreviewsystems.com>
2022-01-05 16:08:27 +0530

    Closes #6 . Implemented Map based interface

---
+++ petclinic-data/src/main/java/om/eternal/petclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package om.eternal.petclinic.services.map;
+
+import om.eternal.petclinic.model.Pet;
+import om.eternal.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet,Long>
+        implements CrudService<Pet,Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
