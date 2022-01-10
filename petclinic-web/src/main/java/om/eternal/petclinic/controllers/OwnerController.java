fc370d9209ef9539b9690e5ed692fb76fb2dd74a
Abhishek-Maurya <abhishek.maurya@coreviewsystems.com>
2022-01-10 17:36:12 +0530

    Just changed my git name

--- petclinic-web/src/main/java/om/eternal/petclinic/controllers/OwnerController.java
+++ petclinic-web/src/main/java/om/eternal/petclinic/controllers/OwnerController.java
@@ -2,7 +2,7 @@ package om.eternal.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+//Just Changed my git name
 @RequestMapping("owners")
 @Controller
 public class OwnerController {
