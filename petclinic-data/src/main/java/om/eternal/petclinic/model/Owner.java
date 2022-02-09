d603da5e01129a94515eb85a23086932dfed60a8
abhishek6174 <abhishek.maurya6174@gmail.com>
2022-02-09 14:12:45 +0530

    Closes #26 added contact info to Owner POJO

--- petclinic-data/src/main/java/om/eternal/petclinic/model/Owner.java
+++ petclinic-data/src/main/java/om/eternal/petclinic/model/Owner.java
@@ -4,6 +4,9 @@ import java.util.Set;
 
 public class Owner extends Person implements Comparable<Owner>{
     private Set<Pet> pets;
+    private String city;
+    private String address;
+    private String telephone;
 
     public Set<Pet> getPets() {
         return pets;
@@ -11,6 +14,30 @@ public class Owner extends Person implements Comparable<Owner>{
 
     public void setPets(Set<Pet> pets) {
         this.pets = pets;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
     }
 
     @Override
