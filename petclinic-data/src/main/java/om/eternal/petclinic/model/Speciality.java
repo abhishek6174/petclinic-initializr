aea4cc23218325842fb63cdb1c877c01ed14198f
abhishek6174 <abhishek.maurya6174@gmail.com>
2022-02-09 13:14:56 +0530

    Closes #25 added Speciality and associated it with Vet

---
+++ petclinic-data/src/main/java/om/eternal/petclinic/model/Speciality.java
@@ -0,0 +1,13 @@
+package om.eternal.petclinic.model;
+
+public class Speciality {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- petclinic-data/src/main/java/om/eternal/petclinic/model/Vet.java
+++ petclinic-data/src/main/java/om/eternal/petclinic/model/Vet.java
@@ -1,6 +1,17 @@
 package om.eternal.petclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person implements Comparable<Vet>{
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 
     @Override
     public int compareTo(Vet o) {
