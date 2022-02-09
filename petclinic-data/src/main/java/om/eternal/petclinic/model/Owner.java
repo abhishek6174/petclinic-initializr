4365349c1319fcfbaf44dc4d25885c7f0809adb5
abhishek6174 <abhishek.maurya6174@gmail.com>
2022-02-09 13:11:27 +0530

    Closes #24 added Pet PetType and visit POJO

--- petclinic-data/src/main/java/om/eternal/petclinic/model/Owner.java
+++ petclinic-data/src/main/java/om/eternal/petclinic/model/Owner.java
@@ -1,6 +1,17 @@
 package om.eternal.petclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person implements Comparable<Owner>{
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 
     @Override
     public int compareTo(Owner o) {
---
+++ petclinic-data/src/main/java/om/eternal/petclinic/model/Visit.java
@@ -0,0 +1,33 @@
+package om.eternal.petclinic.model;
+
+import java.time.LocalDateTime;
+
+public class Visit extends BaseEntity{
+    private LocalDateTime dateTime;
+    private String description;
+    private Pet pet;
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
