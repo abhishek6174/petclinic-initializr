9b3e081d4faee9302427c8bd82a2031f009395ec
abhishek6174 <abhishek.maurya6174@gmail.com>
2022-02-25 15:44:56 +0530

    Closes #33 Created base entity for jpa modeling

--- petclinic-data/src/main/java/om/eternal/petclinic/model/BaseEntity.java
+++ petclinic-data/src/main/java/om/eternal/petclinic/model/BaseEntity.java
@@ -1,8 +1,14 @@
 package om.eternal.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
-
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
 
     public Long getId() {
