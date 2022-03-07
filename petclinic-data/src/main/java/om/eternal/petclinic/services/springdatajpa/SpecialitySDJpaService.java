d8eff732b5a3525968a8a64cbfc407df60852f22
abhishek6174 <abhishek.maurya6174@gmail.com>
2022-03-07 16:07:13 +0530

    Closes #42 #41 #40 #39 #38 #39 implemented jpa repository based service for remaining SD Jpa repository service

---
+++ petclinic-data/src/main/java/om/eternal/petclinic/services/springdatajpa/SpecialitySDJpaService.java
@@ -0,0 +1,43 @@
+package om.eternal.petclinic.services.springdatajpa;
+
+import om.eternal.petclinic.model.Speciality;
+import om.eternal.petclinic.repositories.SpecialityRepository;
+import om.eternal.petclinic.services.SpecialityService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SpecialitySDJpaService implements SpecialityService {
+    private final SpecialityRepository specialityRepository;
+
+    public SpecialitySDJpaService(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialityRepository.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return specialityRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialityRepository.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialityRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialityRepository.deleteById(aLong);
+    }
+}
